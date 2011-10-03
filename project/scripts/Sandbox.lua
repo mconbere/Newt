9aa92a274f95a98ac0fba0a9c16aa44a6b5f5cc8
Brett McLarnon <brett.mclarnon@gmail.com>
2011-10-02 18:54:10 -0700

    New sandbox project.
    The new sandbox project uses Maratis Demo meshes. They should not be used in
    any released binary.

--- project/scripts/Sandbox.lua
+++ project/scripts/Sandbox.lua
@@ -1,36 +1,41 @@
--- Scene script for Sandbox level
--- Author: Brett McLarnon <brett.mclarnon@gmail.com>
-
-Player = getObject("Player")
 Feet = getObject("Feet")
+Player = getObject("Player")
+Jules = getObject("Jules")
 
 function onSceneUpdate()
-  coll = getNumCollisions(Feet)
+	coll = getNumCollisions(Feet)
 
-  move = false
-  if isKeyPressed("LEFT") and (coll > 1) then
-	rotate(Player, {0, 0, 1}, 5)
-	move = true
-  end
-  if isKeyPressed("RIGHT") and (coll > 1) then
-	rotate(Player, {0, 0, 1}, -5)
-	move = true
-  end
-  if isKeyPressed("UP") and (coll > 1) then
-	move = true
-  end
-  if isKeyPressed("SPACE") and (coll > 1) then
-	addCentralForce(Player, {0, 0, 20}, "local")
-  end
-  if move then
-	addCentralForce(Player, {0, -10, 0}, "local")
-	-- changeAnimation(...)
-  end
+	move = 0
+	if coll > 1 then
+		if isKeyPressed("LEFT")  then
+			rotate(Player, {0, 0, 1}, 5)
+		end
+		if isKeyPressed("RIGHT") then
+			rotate(Player, {0, 0, 1}, -5)
+		end
+		if isKeyPressed("UP") then
+			move = move + 1
+		end
+		if isKeyPressed("DOWN") then
+			move = move - 1
+		end
+		
+		if isKeyPressed("SPACE") then
+			addCentralForce(Player, {0, 0, 15}, "local")
+		end
+	end
 
-  -- manual friction (I don't understand)
-  if coll < 2 then
-	setLinearDamping(Player, 0.01)
-  else
-	setLinearDamping(Player, 0.9)
-  end
+	if move == 0 then
+		changeAnimation(Jules, 0)
+	else
+		addCentralForce(Player, {move * 20, 0, 0}, "local")
+		changeAnimation(Jules, 2)
+	end
+
+	-- manual friction
+	if coll < 2 then
+		setLinearDamping(Player, 0.01)
+	else
+		setLinearDamping(Player, 0.9)
+	end	
 end
